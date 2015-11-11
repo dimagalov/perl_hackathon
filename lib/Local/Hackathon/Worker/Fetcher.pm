c6bec74d857c85e1bea7482614cc0d3b0f99a9e3
DoubleDi <ddenis1@yandex.ru>
2015-11-11 21:05:35 +0300

    done Fetcher

--- lib/Local/Hackathon/Worker/Fetcher.pm
+++ lib/Local/Hackathon/Worker/Fetcher.pm
@@ -1,10 +1,19 @@
 package Local::Hackathon::Worker::Fetcher;
 
 use Mouse::Role;
+use LWP::Simple;
+use DDP;
+use 5.018;
 
 has '+source',       default => 'fetch';
-has '+destination',  default => 'title';
+has '+destination',  default => 'og';
 
-# sub process { ... }
-
+sub process 
+{
+    my ($self, $task) = @_;
+    $task->{'HTML'} = get $task->{'URL'};
+    die "Couldn't get $task->{'URL'}" unless defined $task->{'HTML'};
+    say "got HTML";
+    return $task;
+}
 1;
