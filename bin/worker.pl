c703dbefcca71fc76f6f25a0360504952c38d046
DoubleDi <ddenis1@yandex.ru>
2015-11-11 21:19:22 +0300

    Update worker.pl

--- bin/worker.pl
+++ bin/worker.pl
@@ -2,8 +2,22 @@
 
 use strict;
 use lib::abs '../lib';
-#BEGIN{GetOptions}
-use Local::Hackathon::Worker $ARGV[0];
+use Getopt::Long;
+use DDP;
 
-my $worker = Local::Hackathon::Worker->new();
-$worker->run();
+#BEGIN{GetOptions}
+use Local::Hackathon::Worker shift;
+my $host = '192.168.0.65';
+my $port = '3456';
+# p @ARGV;
+# GetOptions("host=s" => \$host, "port=i" => \$port) or die;
+while() {
+    eval {
+        p $host;
+        p $port;
+        my $promoter_client = Local::Hackathon::Client->new(host => $host,port => $port);
+        my $worker = Local::Hackathon::Worker->new(client=>$promoter_client);
+        $worker->run();
+    1} or do { warn "error".$@ };
+    sleep 1;
+}
