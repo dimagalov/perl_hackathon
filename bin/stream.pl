21724a36b459c9e75c4a4842eae1e9d7a359c73e
Max <max.shegai@gmail.com>
2015-11-11 20:31:49 +0300

    stream.pl -- fetcher

--- bin/stream.pl
+++ bin/stream.pl
@@ -5,6 +5,11 @@ use XML::Atom::Stream ();
 use DDP;
 use Data::Dumper;
 use POSIX 'strftime';
+use Getopt::Long;
+
+use Local::Hackathon::Client;
+
+
 
 my $client = XML::Atom::Stream->new(
 	callback  => \&callback,
@@ -12,13 +17,29 @@ my $client = XML::Atom::Stream->new(
 	# debug     => 1,
 	timeout   => 30,
 );
+
+my $host = 'localhost';
+my $port = 3456;
+
+GetOptions("host=s" => \$host,
+			"port=i" => \$port);
+
+my $promoter_client = Local::Hackathon::Client->new(
+	host => $host,
+	port => $port
+);
+
+
 $client->connect('http://atom.services.livejournal.com/atom-stream.xml');
 
 sub callback {
+	my %h;
 	my ($atom) = @_;
 	eval {
 		for my $ent ($atom->entries) {
 			printf "%s %s # %s\n",strftime("%Y-%m-%dT%H:%M:%S",localtime()), $ent->link->href, $ent->title;
+			$h{URL} = $ent->link->href;
+			$promoter_client->put('fetch',\%h);
 		}
 	1} or do {
 		warn "$@";
